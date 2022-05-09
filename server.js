265c57530cd35389ff7ea1fdbc913f21f00e460d
Mo Yang <moyang313@gmail.com>
2022-05-09 00:18:39 -0400

    add view functions

--- server.js
+++ server.js
@@ -24,63 +24,75 @@ const db = mysql.createConnection(
 );
 
 
-// const managerQuestions = () => {
-//     return inquirer.prompt([
-//         {
-//             type: "list",
-//             name: "selection",
-//             message: "What would you like to do?",
-//             choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
-//             validate: selectionInput => {
-//                 if (selectionInput) {
-//                     return true;
-//                 } else {
-//                     console.log('You need to make a selection!');
-//                     return false;
-//                 }
-//             }
-//         },
-//         {
-//             type: "input",
-//             name: "officeNumber",
-//             message: "Please enter an office number (Required)",
-//             validate: officeNumberInput => {
-//                 if (officeNumberInput) {
-//                 return true;
-//                 } else {
-//                 console.log('Please enter an office number!');
-//                 return false;
-//                 }
-//             }
-//             },
-//         {
-//         type: "list",
-//         name: "addTeamMember",
-//         message: "Please choose if you would like to add additional team members:",
-//         choices: ['Add Engineer', 'Add Intern', 'No more members'],
-//         validate: addTeamMemberInput => {
-//             if (addTeamMemberInput) {
-//                 return true;
-//             } else {
-//                 console.log('You need to make a selection!');
-//                 return false;
-//             }
-//         }
-//         }
-//     ])
-//     .then ((managerAnswers) => {
-//         switch(managerAnswers.addTeamMember) {
-//             case 'Add Engineer':
-//                 engineerQuestions();
-//                 break;
-//             case 'Add Intern':
-//                 internQuestions();
-//                 break;
-//             default: 
-//             writeToFile('dist/index.html', generatePage(team))
-//         }
-//     }
-//     )
-// };
+const initialQuestion = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "selection",
+            message: "What would you like to do?",
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
+            validate: selectionInput => {
+                if (selectionInput) {
+                    return true;
+                } else {
+                    console.log('You need to make a selection!');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then ((initialAnswer) => {
+        switch(initialAnswer.selection) {
+            case 'view all departments':
+                viewDepartments();
+                break;
+            case 'view all roles':
+                viewRoles();
+                break;
+            case 'view all employees':
+                viewEmployees();
+                break;
+            default: 
+            initialQuestion();
+        }
+    }
+    )
+};
 
-// managerQuestions();
+const viewDepartments = () => {
+let sql = `SELECT * FROM departments`
+
+db.query(sql, (err, res) => {
+    if (err) throw err;
+
+    console.table(res);
+
+    initialQuestion ();
+});
+}
+
+const viewRoles = () => {
+let sql = `SELECT * FROM roles`
+
+db.query(sql, (err, res) => {
+    if (err) throw err;
+
+    console.table(res);
+
+    initialQuestion ();
+});
+}
+
+const viewEmployees = () => {
+let sql = `SELECT * FROM employees`
+
+db.query(sql, (err, res) => {
+    if (err) throw err;
+
+    console.table(res);
+
+    initialQuestion ();
+});
+}
+
+initialQuestion();
